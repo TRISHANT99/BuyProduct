0ba6e6f86de9c6cd221131cbe1df096203f9d3d6
Trishant99 <trishantverma22452@gmail.com>
2026-07-04 19:05:21 +0530

    first commit

---
+++ backend/routes/order.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { placeOrder } from "../controllers/orderController.js";
+const router = express.Router();
+router.post("/place", placeOrder);
+export default router;
